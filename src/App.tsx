628273b9a005449091189ebf5a02530146f0b9bf
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2023-01-03 15:58:59 -0300

    codigo atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'styled-components'
-import { Transactions } from "./pages/Transactions"
+import { TransactionsProvider } from './contexts/TransactionsContext'
+import { Transactions } from './pages/Transactions'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
@@ -7,7 +8,10 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Transactions />
+
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
   )
 }
